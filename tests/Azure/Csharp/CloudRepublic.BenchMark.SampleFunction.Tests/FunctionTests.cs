49081f9e0c55469e133dd4c61f9f664e2500a570
Florian Schaal <f.schaal@caesar.nl>
2019-06-05 10:18:04 +0200

    init

---
+++ tests/Azure/Csharp/CloudRepublic.BenchMark.SampleFunction.Tests/FunctionTests.cs
@@ -0,0 +1,39 @@
+﻿using CloudRepublic.BenchMark.SampleFunction;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BenchCloud.CSharp.Tests
+{
+    public class FunctionTests
+    {
+        private readonly ILogger logger = TestFactory.CreateLogger();
+        
+        [Fact]
+        public async Task FunctionShouldReturnProvidedName()
+        {
+            string nameValue = "BenchCloud";
+
+            var request = TestFactory.CreateHttpRequest("name", nameValue);
+            var response = await Trigger.Run(request, logger);
+
+            var responseObject = Assert.IsType<OkObjectResult>(response);
+            Assert.Equal($"Hello, {nameValue}", responseObject.Value);
+        }
+
+        [Fact]
+        public async Task FunctionShouldReturnBadRequest()
+        {
+            var request = TestFactory.CreateInvalidHttpRequest();
+
+            var response = await Trigger.Run(request, logger);
+
+            var responseObject = Assert.IsType<BadRequestObjectResult>(response);
+            Assert.Equal("Please pass a name on the query string", responseObject.Value);
+        }
+    }
+}
---
+++ tests/Azure/Csharp/CloudRepublic.BenchMark.SampleFunction.Tests/TestFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchCloud.CSharp.Tests
+{
+    public class TestFactory
+    {
+        public static DefaultHttpRequest CreateHttpRequest(string queryKey,string queryValue)
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>() { { queryKey, queryValue} })
+            };
+        }
+
+        public static DefaultHttpRequest CreateInvalidHttpRequest()
+        {
+            return new DefaultHttpRequest(new DefaultHttpContext());
+        }
+
+
+        public static ILogger CreateLogger()
+        {
+            return NullLoggerFactory.Instance.CreateLogger("Null Logger");
+        }
+    }
+}
