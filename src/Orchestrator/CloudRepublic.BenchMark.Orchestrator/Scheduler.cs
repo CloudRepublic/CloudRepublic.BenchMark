e577ed624a55d484d82ae953594ee90225a029dd
Florian Schaal <f.schaal@foxtail-apps.com>
2019-06-06 21:52:34 +0200

    Added DEBUG trigger to scheduler function

--- src/Orchestrator/CloudRepublic.BenchMark.Orchestrator/Scheduler.cs
+++ src/Orchestrator/CloudRepublic.BenchMark.Orchestrator/Scheduler.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 namespace CloudRepublic.BenchMark.Orchestrator
@@ -7,7 +9,13 @@ namespace CloudRepublic.BenchMark.Orchestrator
     public static class Scheduler
     {
         [FunctionName("BenchMarkScheduler")]
-        public static async Task RunScheduled([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer,
+        public static async Task RunScheduled(
+#if DEBUG
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]
+            HttpRequest req,
+#else
+            [TimerTrigger("0 0 */4 * * *")] TimerInfo myTimer,
+#endif
             [OrchestrationClient] DurableOrchestrationClient starter,
             ILogger log)
         {
