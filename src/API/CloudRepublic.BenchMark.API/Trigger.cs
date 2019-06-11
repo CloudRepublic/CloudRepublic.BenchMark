c3681ae59f0c65b1668deb4440efaf00897957ee
Florian Schaal <f.schaal@foxtail-apps.com>
2019-06-11 11:48:16 +0200

    Added API function project

---
+++ src/API/CloudRepublic.BenchMark.API/Trigger.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CloudRepublic.BenchMark.API
+{
+    public static class Trigger
+    {
+        [FunctionName("Trigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+            return new OkResult();
+        }
+    }
+}
