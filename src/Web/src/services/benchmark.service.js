e499d5266ebe059ffc87592004cf3ddd49ba3631
Martin Ligtenberg <martin@ligtenberg.digital>
2022-11-10 20:45:23 +0100

    trying to fix the frontend service

--- src/Web/src/services/benchmark.service.js
+++ src/Web/src/services/benchmark.service.js
@@ -2,12 +2,7 @@ export const benchMarkService = { getBenchMarkData };
 
 async function getBenchMarkData(cloudProvider, hostingEnvironment, runtime) {
   let response = await fetch(
-    `${
-    process.env.VUE_APP_API
-    }?cloudProvider=${cloudProvider}&hostingEnvironment=${hostingEnvironment}&runtime=${runtime}`,
-    {
-      headers: { 'Ocp-Apim-Subscription-Key': process.env.VUE_APP_API_KEY }
-    }
+    `/api/statistics?cloudProvider=${cloudProvider}&hostingEnvironment=${hostingEnvironment}&runtime=${runtime}`
   );
   return handleResponse(response);
 }
